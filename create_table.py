ea7dcebb0bd590d42f33ca4a0de7ed027c7fa3cc
Antonio Vilela Pereira <antonio.vilela.pereira@cern.ch>
2020-08-06 21:09:51 +0200

    Add background mixing notebook, update create_table script with mixing option, add background plots in data inspection notebook.

--- create_table.py
+++ create_table.py
@@ -9,10 +9,14 @@ import scipy.constants
 import h5py
 import argparse
 
+from select_events import *
+
 parser = argparse.ArgumentParser(description = 'Creates data table from ntuple')
 parser.add_argument('--files', help = 'File paths' )
 parser.add_argument('--label', help = 'Label suffix' )
 parser.add_argument('--apply_exclusive', dest = 'apply_exclusive', action = 'store_true', required = False, help = '' )
+parser.add_argument('--random_protons', dest = 'random_protons', action = 'store_true', required = False, help = '' )
+parser.add_argument('--resample_factor', dest = 'resample_factor', type = int, required = False, default = -1, help = '' )
 parser.add_argument('-n', '--events', dest = 'events', type = int, required = False, default = -1, help = 'Number of events to process' )
 #parser.add_argument('-v', '--verbose', action = 'store_true', dest = 'verbose', required = False, help = 'Enable verbose' )
 args = parser.parse_args()
@@ -25,159 +29,58 @@ print ( "Label: " + label_ )
 apply_exclusive_ = False
 if hasattr( args, 'apply_exclusive') and args.apply_exclusive: apply_exclusive_ = args.apply_exclusive
 print ( "Apply exclusive selection: {}".format( apply_exclusive_ ) )
+random_protons_ = False
+if hasattr( args, 'random_protons') and args.random_protons: random_protons_ = args.random_protons
+print ( "Random protons: {}".format( random_protons_ ) )
+resample_factor_ = -1
+if hasattr( args, 'resample_factor'): resample_factor_ = args.resample_factor
+print ( "Resample factor: {}".format( resample_factor_ ) )
 maxEvents_ = None
 if hasattr( args, 'events' ) and args.events > 0: maxEvents_ = args.events
 print ( "Number of events to process: {}".format( "All" if maxEvents_ is None else maxEvents_ ) )
 
-def select_events( events, apply_exclusive=True ):
+resample_ = False
+if resample_factor_ > 1: resample_ = True
 
-    selections_ = []
-    counts_ = []
+np.random.seed( 42 )
 
-    msk_2muons = ( events.nMuonCand >= 2 )
-    events_2muons = events[msk_2muons]    
+columns = ( "Run", "LumiSection", "BX", "EventNum", "CrossingAngle",
+            "MultiRP", "Arm", "RPId1", "RPId2", "TrackX1", "TrackY1", "TrackX2", "TrackY2",
+            "Xi", "T", "ThX", "ThY", "Time",
+            "TrackThX_SingleRP", "TrackThY_SingleRP",
+            "Track1ThX_MultiRP", "Track1ThY_MultiRP", "Track2ThX_MultiRP", "Track2ThY_MultiRP",
+            "Muon0Pt", "Muon0Eta", "Muon0Phi", "Muon0VtxZ", "Muon1Pt", "Muon1Eta", "Muon1Phi", "Muon1VtxZ",
+            "PrimVertexZ", "InvMass", "ExtraPfCands", "Acopl", "XiMuMuPlus", "XiMuMuMinus" )
+protons_keys = {}
+for col_ in columns:
+    protons_keys[ col_ ] = col_
+protons_keys[ "MultiRP" ] = "ismultirp"
+protons_keys[ "Arm" ] = "arm"
+protons_keys[ "RPId1" ] = "rpid1"
+protons_keys[ "RPId2" ] = "rpid2"
+protons_keys[ "TrackX1" ] = "trackx1"
+protons_keys[ "TrackY1" ] = "tracky1"
+protons_keys[ "TrackX2" ] = "trackx2"
+protons_keys[ "TrackY2" ] = "tracky2"
+protons_keys[ "Xi" ] = "xi"
+protons_keys[ "T" ] = "t"
+protons_keys[ "Time" ] = "time"
+protons_keys[ "TrackThX_SingleRP" ] = "trackthx_single"
+protons_keys[ "TrackThY_SingleRP" ] = "trackthy_single"
+protons_keys[ "Track1ThX_MultiRP" ] = "trackthx_multi1"
+protons_keys[ "Track1ThY_MultiRP" ] = "trackthy_multi1"
+protons_keys[ "Track2ThX_MultiRP" ] = "trackthx_multi2"
+protons_keys[ "Track2ThY_MultiRP" ] = "trackthy_multi2"
+protons_keys[ "ExtraPfCands" ] = "nExtraPfCandPV3"
 
-    dphi = events_2muons.MuonCand.phi[:,0] - events_2muons.MuonCand.phi[:,1]
-    
-    dphi = np.where( dphi >=  scipy.constants.pi, dphi - 2*scipy.constants.pi, dphi)
-    dphi = np.where( dphi <  -scipy.constants.pi, dphi + 2*scipy.constants.pi, dphi)
-    acopl = 1. - np.abs(dphi)/scipy.constants.pi
-
-    events_2muons["Acopl"] = acopl
-
-    m1 = events_2muons.MuonCand[:,0]
-    m2 = events_2muons.MuonCand[:,1]
-
-    invariant_mass = np.sqrt( 2*m1.pt*m2.pt*( np.cosh(m1.eta - m2.eta) - np.cos(m1.phi - m2.phi) ) )
-
-    events_2muons["InvMass"] = invariant_mass
-
-    energy_com = 13000.
-    xi_mumu_plus = (1./energy_com) * ( m1.pt*np.exp(m1.eta) + m2.pt*np.exp(m2.eta) )
-    xi_mumu_minus = (1./energy_com) * ( m1.pt*np.exp(-m1.eta) + m2.pt*np.exp(-m2.eta) )
-
-    events_2muons["XiMuMuPlus"] = xi_mumu_plus
-    events_2muons["XiMuMuMinus"] = xi_mumu_minus
-
-    pfCands_ = events_2muons.PfCand
-
-    pfCands_["dR_0"] = np.sqrt( ( pfCands_.eta - events_2muons.MuonCand.eta[:,0] )**2 + ( pfCands_.phi - events_2muons.MuonCand.phi[:,0] )**2 )
-    pfCands_["dR_1"] = np.sqrt( ( pfCands_.eta - events_2muons.MuonCand.eta[:,1] )**2 + ( pfCands_.phi - events_2muons.MuonCand.phi[:,1] )**2 )
-
-    pfCands_sel1_ = pfCands_[
-                    pfCands_.fromPV == 3.0 
-                    ]
-    pfCands_sel2_ = pfCands_sel1_[
-                    pfCands_sel1_.dR_0 > 0.3 
-                    ]
-    pfCands_sel3_ = pfCands_sel2_[
-                    pfCands_sel2_.dR_1 > 0.3 
-                    ]
-    events_2muons[ "nExtraPfCandPV3" ] = ak.num( pfCands_sel3_ )
-    
-    msk_muon = ( np.array( events_2muons.MuonCand.pt[:,0] >= 50. ) & np.array( events_2muons.MuonCand.pt[:,1] >= 50. ) &
-                 np.array( events_2muons.MuonCand.istight[:,0] == 1 ) & np.array( events_2muons.MuonCand.istight[:,1] == 1 ) &
-                 np.array( ( events_2muons.MuonCand.charge[:,0] * events_2muons.MuonCand.charge[:,1] ) == -1 ) )
-    selections_.append( "Muon" )
-    counts_.append( np.sum( msk_muon ) )
-    
-    msk_vtx = msk_muon & ( 
-        np.array( np.abs( events_2muons.PrimVertexCand.z[:,0] ) <= 15. ) &
-        np.array( np.abs( events_2muons.MuonCand.vtxz[:,0] - events_2muons.PrimVertexCand.z[:,0] ) <= 0.02 ) &
-        np.array( np.abs( events_2muons.MuonCand.vtxz[:,1] - events_2muons.PrimVertexCand.z[:,0] ) <= 0.02 ) 
-        )
-    selections_.append( "Vertex" )
-    counts_.append( np.sum( msk_vtx ) )
-    
-    events_sel = None
-    if apply_exclusive:
-        msk_excl = msk_vtx & ( np.array( events_2muons["InvMass"] >= 110. ) & 
-                               np.array( events_2muons["Acopl"] <= 0.009 ) & 
-                               np.array( events_2muons["nExtraPfCandPV3"] <= 1 ) )
-        selections_.append( "Exclusive" )
-        counts_.append( np.sum( msk_excl ) )
-
-        events_sel = events_2muons[ msk_excl ]
-    else:
-        events_sel = events_2muons[ msk_vtx ]  
-
-    selections_ = np.array( selections_ )
-    counts_ = np.array( counts_ )
-    #print ( selections_ )
-    #print ( counts_ )    
-    
-    return ( events_sel, selections_, counts_ )
-        
-def select_protons(events):
-    #protons_ = events.ProtCand[
-    #    events.ProtCand.ismultirp == 1
-    #    ]
-
-    protons_ = events.ProtCand
-
-    protons_["Run"] = events[ "Run" ]
-    protons_["LumiSection"] = events[ "LumiSection" ]
-    protons_["BX"] = events[ "BX" ]
-    protons_["EventNum"] = events[ "EventNum" ]
-    
-    protons_["CrossingAngle"] = events[ "CrossingAngle" ]
-    
-    protons_["Muon0Pt"] = events.MuonCand.pt[:,0]
-    protons_["Muon0Eta"] = events.MuonCand.eta[:,0]
-    protons_["Muon0Phi"] = events.MuonCand.phi[:,0]
-    protons_["Muon0VtxZ"] = events.MuonCand.vtxz[:,0]
-    protons_["Muon1Pt"] = events.MuonCand.pt[:,1]
-    protons_["Muon1Eta"] = events.MuonCand.eta[:,1]
-    protons_["Muon1Phi"] = events.MuonCand.phi[:,1]
-    protons_["Muon1VtxZ"] = events.MuonCand.vtxz[:,1]
-
-    protons_["PrimVertexZ"] = events.PrimVertexCand.z[:,0]
-    
-    protons_["InvMass"] = events[ "InvMass" ]
-    protons_["nExtraPfCandPV3"] = events[ "nExtraPfCandPV3" ]
-    protons_["Acopl"] = events[ "Acopl" ]
-
-    protons_["XiMuMuPlus"] = events[ "XiMuMuPlus" ]
-    protons_["XiMuMuMinus"] = events[ "XiMuMuMinus" ]
-    
-    msk_num_prot = ( ak.num( protons_.xi ) > 0 )
-    protons_ = protons_[ msk_num_prot ]
-    
-    print ( len(protons_) )
-    
-    return protons_
-
-protons_Run_list = []
-protons_LumiSection_list = []
-protons_BX_list = []
-protons_EventNum_list = []
-protons_CrossingAngle_list = []
-protons_xi_list = []
-protons_ismultirp_list = []
-protons_arm_list = []
-protons_rpid1_list = []
-protons_rpid2_list = []
-protons_trackx1_list = []
-protons_tracky1_list = []
-protons_trackx2_list = []
-protons_tracky2_list = []
-protons_Muon0Pt_list = []
-protons_Muon0Eta_list = []
-protons_Muon0Phi_list = []
-protons_Muon0VtxZ_list = []
-protons_Muon1Pt_list = []
-protons_Muon1Eta_list = []
-protons_Muon1Phi_list = []
-protons_Muon1VtxZ_list = []
-protons_PrimVertexZ_list = []
-protons_InvMass_list = []
-protons_nExtraPfCandPV3_list = []
-protons_Acopl_list = []
-protons_XiMuMuPlus_list = []
-protons_XiMuMuMinus_list = []
+protons_list = {}
+for col_ in columns:
+    protons_list[ col_ ] = []
 
 selections = None
 counts = None
+
+counts_label_protons_ = "Protons" if not random_protons_ else "ProtonsRnd"
 
 for file_ in fileNames_:
     print ( file_ ) 
@@ -199,8 +102,11 @@ for file_ in fileNames_:
         
         #events_sel_ = select_events( events_, apply_exclusive_ )
         events_sel_, selections_, counts_ = select_events( events_, apply_exclusive=apply_exclusive_ )
-        print ( selections_ )
-        print ( counts_ )
+
+        # Repeat events by resample factor
+        if resample_:
+            counts_ = counts_ * resample_factor_
+
         if selections is None:
             selections = selections_
             counts = counts_
@@ -212,182 +118,58 @@ for file_ in fileNames_:
                 msk_selections |= ( selections == key )
             counts[ msk_selections ] += counts_
 
-        protons_ = select_protons( events_sel_ )
+        # Repeat events by resample factor
+        if resample_:
+            events_sel_ = ak.concatenate( ( [events_sel_] * resample_factor_ ), axis=0 )
+            
+        # Randomize proton arrays
+        if random_protons_:
+            protons_sel_ = events_sel_.ProtCand
+        
+            index_rnd_ = np.random.permutation( len( events_sel_ ) )
+        
+            protons_rnd_ = protons_sel_[ index_rnd_ ]
+        
+            events_sel_[ "ProtCandRnd" ] = protons_rnd_    
+    
+            print ( ak.num( events_sel_.ProtCand ) )
+            print ( ak.num( events_sel_.ProtCandRnd ) )    
+
+        #protons_ = select_protons( events_sel_ )
+        protons_ = None
+        if not random_protons_: protons_ = select_protons( events_sel_, "ProtCand" )
+        else:                   protons_ = select_protons( events_sel_, "ProtCandRnd" )    
+
         counts_protons_ = len( protons_ )
-        if not "Protons" in selections:
-            selections = np.concatenate( ( selections, np.array( ["Protons"], dtype='S' ) ) )
+        if not counts_label_protons_ in selections:
+            selections = np.concatenate( ( selections, np.array( [ counts_label_protons_ ] ) ) )
             counts = np.concatenate( ( counts, np.array( [counts_protons_] ) ) )
         else:    
-            counts[ selections == 'Protons'] += counts_protons_ 
+            counts[ selections == counts_label_protons_ ] += counts_protons_ 
+
         print ( selections )
         print ( counts )
 
-        protons_Run = ak.flatten( protons_.Run )
-        protons_LumiSection = ak.flatten( protons_.LumiSection )
-        protons_BX = ak.flatten( protons_.BX )
-        protons_EventNum = ak.flatten( protons_.EventNum )
-        protons_CrossingAngle = ak.flatten( protons_.CrossingAngle )
-        protons_xi = ak.flatten( protons_.xi )
-        protons_ismultirp = ak.flatten( protons_.ismultirp )
-        protons_arm = ak.flatten( protons_.arm )
-        protons_rpid1 = ak.flatten( protons_.rpid1 )
-        protons_rpid2 = ak.flatten( protons_.rpid2 )
-        protons_trackx1 = ak.flatten( protons_.trackx1 )
-        protons_tracky1 = ak.flatten( protons_.tracky1 )
-        protons_trackx2 = ak.flatten( protons_.trackx2 )
-        protons_tracky2 = ak.flatten( protons_.tracky2 )   
-        protons_Muon0Pt = ak.flatten( protons_.Muon0Pt )
-        protons_Muon0Eta = ak.flatten( protons_.Muon0Eta )
-        protons_Muon0Phi = ak.flatten( protons_.Muon0Phi )
-        protons_Muon0VtxZ = ak.flatten( protons_.Muon0VtxZ )
-        protons_Muon1Pt = ak.flatten( protons_.Muon1Pt )
-        protons_Muon1Eta = ak.flatten( protons_.Muon1Eta )
-        protons_Muon1Phi = ak.flatten( protons_.Muon1Phi )
-        protons_Muon1VtxZ = ak.flatten( protons_.Muon1VtxZ )
-        protons_PrimVertexZ = ak.flatten( protons_.PrimVertexZ )
-        protons_InvMass = ak.flatten( protons_.InvMass )
-        protons_nExtraPfCandPV3 = ak.flatten( protons_.nExtraPfCandPV3 )
-        protons_Acopl = ak.flatten( protons_.Acopl )
-        protons_XiMuMuPlus = ak.flatten( protons_.XiMuMuPlus )
-        protons_XiMuMuMinus = ak.flatten( protons_.XiMuMuMinus )
-        
-        print ( "Protons (flattened): {}".format( len(protons_xi) ) )
-        
-        protons_Run_list.append( protons_Run )
-        protons_LumiSection_list.append( protons_LumiSection )
-        protons_BX_list.append( protons_BX )
-        protons_EventNum_list.append( protons_EventNum )
-        protons_CrossingAngle_list.append( protons_CrossingAngle )
-        protons_xi_list.append( protons_xi )
-        protons_ismultirp_list.append( protons_ismultirp )
-        protons_arm_list.append( protons_arm )
-        protons_rpid1_list.append( protons_rpid1 )
-        protons_rpid2_list.append( protons_rpid2 )
-        protons_trackx1_list.append( protons_trackx1 )
-        protons_tracky1_list.append( protons_tracky1 )
-        protons_trackx2_list.append( protons_trackx2 )
-        protons_tracky2_list.append( protons_tracky2 )
-        protons_Muon0Pt_list.append( protons_Muon0Pt )
-        protons_Muon0Eta_list.append( protons_Muon0Eta )
-        protons_Muon0Phi_list.append( protons_Muon0Phi )
-        protons_Muon0VtxZ_list.append( protons_Muon0VtxZ )
-        protons_Muon1Pt_list.append( protons_Muon1Pt )
-        protons_Muon1Eta_list.append( protons_Muon1Eta )
-        protons_Muon1Phi_list.append( protons_Muon1Phi )
-        protons_Muon1VtxZ_list.append( protons_Muon1VtxZ )
-        protons_PrimVertexZ_list.append( protons_PrimVertexZ )
-        protons_InvMass_list.append( protons_InvMass )
-        protons_nExtraPfCandPV3_list.append( protons_nExtraPfCandPV3 )
-        protons_Acopl_list.append( protons_Acopl )
-        protons_XiMuMuPlus_list.append( protons_XiMuMuPlus )
-        protons_XiMuMuMinus_list.append( protons_XiMuMuMinus )
-        
-    print ( selections )
-    print ( counts )    
+        protons_flatten_ = {}
+        for col_ in columns:
+            protons_list[ col_ ].append( ak.flatten( protons_[ protons_keys[ col_ ] ] ) )
+
     root_.close()
 
-protons_Run_all             = np.array( np.concatenate( protons_Run_list ) )
-protons_LumiSection_all     = np.array( np.concatenate( protons_LumiSection_list ) )
-protons_BX_all              = np.array( np.concatenate( protons_BX_list ) )
-protons_EventNum_all        = np.array( np.concatenate( protons_EventNum_list ) )
-protons_CrossingAngle_all   = np.array( np.concatenate( protons_CrossingAngle_list ) )
-protons_xi_all              = np.array( np.concatenate( protons_xi_list ) )
-protons_ismultirp_all       = np.array( np.concatenate( protons_ismultirp_list ) )
-protons_arm_all             = np.array( np.concatenate( protons_arm_list ) )
-protons_rpid1_all           = np.array( np.concatenate( protons_rpid1_list ) )
-protons_rpid2_all           = np.array( np.concatenate( protons_rpid2_list ) )
-protons_trackx1_all         = np.array( np.concatenate( protons_trackx1_list ) )
-protons_tracky1_all         = np.array( np.concatenate( protons_tracky1_list ) )
-protons_trackx2_all         = np.array( np.concatenate( protons_trackx2_list ) )
-protons_tracky2_all         = np.array( np.concatenate( protons_tracky2_list ) )
-protons_Muon0Pt_all         = np.array( np.concatenate( protons_Muon0Pt_list ) )
-protons_Muon0Eta_all        = np.array( np.concatenate( protons_Muon0Eta_list ) )
-protons_Muon0Phi_all        = np.array( np.concatenate( protons_Muon0Phi_list ) )
-protons_Muon0VtxZ_all       = np.array( np.concatenate( protons_Muon0VtxZ_list ) )
-protons_Muon1Pt_all         = np.array( np.concatenate( protons_Muon1Pt_list ) )
-protons_Muon1Eta_all        = np.array( np.concatenate( protons_Muon1Eta_list ) )
-protons_Muon1Phi_all        = np.array( np.concatenate( protons_Muon1Phi_list ) )
-protons_Muon1VtxZ_all       = np.array( np.concatenate( protons_Muon1VtxZ_list ) )
-protons_PrimVertexZ_all     = np.array( np.concatenate( protons_PrimVertexZ_list ) )
-protons_InvMass_all         = np.array( np.concatenate( protons_InvMass_list ) )
-protons_nExtraPfCandPV3_all = np.array( np.concatenate( protons_nExtraPfCandPV3_list ) )
-protons_Acopl_all           = np.array( np.concatenate( protons_Acopl_list ) )
-protons_XiMuMuPlus_all      = np.array( np.concatenate( protons_XiMuMuPlus_list ) )
-protons_XiMuMuMinus_all     = np.array( np.concatenate( protons_XiMuMuMinus_list ) )    
+protons_flatten_all = {}
+for col_ in columns:
+    protons_flatten_all[ col_ ] = np.array( np.concatenate( protons_list[ col_ ] ) )
     
-print ( "Protons (flattened): {}".format( len(protons_xi_all) ) )
+print ( "Protons (flattened): {}".format( len( protons_flatten_all["Xi"] ) ) )
 
-print ( protons_Run_all )
-print ( protons_LumiSection_all )
-print ( protons_BX_all )
-print ( protons_EventNum_all )
-print ( protons_CrossingAngle_all )
-print ( protons_xi_all )
-print ( protons_ismultirp_all )
-print ( protons_arm_all )
-print ( protons_rpid1_all )
-print ( protons_rpid2_all )
-print ( protons_trackx1_all )
-print ( protons_tracky1_all )
-print ( protons_trackx2_all )
-print ( protons_tracky2_all )
-print ( protons_Muon0Pt_all )
-print ( protons_Muon0Eta_all )
-print ( protons_Muon0Phi_all )
-print ( protons_Muon0VtxZ_all )
-print ( protons_Muon1Pt_all )
-print ( protons_Muon1Eta_all )
-print ( protons_Muon1Phi_all )
-print ( protons_Muon1VtxZ_all )
-print ( protons_PrimVertexZ_all )
-print ( protons_InvMass_all )
-print ( protons_nExtraPfCandPV3_all )
-print ( protons_Acopl_all )
-print ( protons_XiMuMuPlus_all )
-print ( protons_XiMuMuMinus_all )
-
-data_ = np.c_[
-    protons_Run_all,
-    protons_LumiSection_all,
-    protons_BX_all,
-    protons_EventNum_all,
-    protons_CrossingAngle_all,
-    protons_xi_all,
-    protons_ismultirp_all,
-    protons_arm_all,
-    protons_rpid1_all,
-    protons_rpid2_all,
-    protons_trackx1_all,
-    protons_tracky1_all,
-    protons_trackx2_all,
-    protons_tracky2_all,
-    protons_Muon0Pt_all,
-    protons_Muon0Eta_all,
-    protons_Muon0Phi_all,
-    protons_Muon0VtxZ_all,
-    protons_Muon1Pt_all,
-    protons_Muon1Eta_all,
-    protons_Muon1Phi_all,
-    protons_Muon1VtxZ_all,
-    protons_PrimVertexZ_all,
-    protons_InvMass_all,
-    protons_nExtraPfCandPV3_all,
-    protons_Acopl_all,
-    protons_XiMuMuPlus_all,
-    protons_XiMuMuMinus_all
-    ]
-
+data_ = np.stack( list( protons_flatten_all.values() ), axis=1 )
+print ( data_ )
 print ( data_.shape )
-
-columns_ = np.array( ("Run", "LumiSection", "BX", "EventNum", "CrossingAngle",
-                      "Xi", "MultiRP", "Arm", "RPId1", "RPId2", "TrackX1", "TrackY1", "TrackX2", "TrackY2",
-                      "Muon0Pt", "Muon0Eta", "Muon0Phi", "Muon0VtxZ", "Muon1Pt", "Muon1Eta", "Muon1Phi", "Muon1VtxZ",
-                      "PrimVertexZ", "InvMass", "ExtraPfCands", "Acopl", "XiMuMuPlus", "XiMuMuMinus"), dtype='S' )
+columns_ = np.array( columns, dtype='S' )
 print ( columns_ )
 
 event_counts_ = counts
 print ( event_counts_ )
-
 selections_ = np.array( selections, dtype='S' )
 print ( selections_ )
 
--- sub_condor.csh
+++ sub_condor.csh
@@ -4,21 +4,32 @@ source /afs/cern.ch/work/a/antoniov/env/uproot4/bin/activate.csh
 
 set file=$1
 set label=$2
-set option=$3
+set option1=$3
+set option2=$4
+set option3=$5
+set option4=$6
 set EXEC=/eos/home-a/antoniov/SWAN_projects/pps-dilepton-analysis
 set OUTPUT=/eos/home-a/antoniov/SWAN_projects/pps-dilepton-analysis/output
 echo "file: "$file
 echo "label: "$label
-echo "option: "$option
+echo "option: "$option1
+echo "option: "$option2
+echo "option: "$option3
+echo "option: "$option4
 echo $EXEC
 echo $OUTPUT
+
+if ( ! $?PYTHONPATH ) then
+    setenv PYTHONPATH ${EXEC}
+else
+    setenv PYTHONPATH ${PYTHONPATH}:${EXEC}
+endif
+echo PYTHONPATH set to $PYTHONPATH 
 env
 
 echo $PWD
 
 echo 'Running...'
-echo python $EXEC/create_table.py --files=$file --label=$label $option
-python $EXEC/create_table.py --files=$file --label=$label $option
-
+echo python $EXEC/create_table.py --files=$file --label=$label $option1 $option2 $option3 $option4
+python $EXEC/create_table.py --files=$file --label=$label $option1 $option2 $option3 $option4
 cp *.h5 $OUTPUT
-
