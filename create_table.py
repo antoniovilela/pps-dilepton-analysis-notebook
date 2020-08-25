1dec5a44b0925eed4abfa4a9452e4033264e48ac
Antonio Vilela Pereira <antonio.vilela.pereira@cern.ch>
2020-08-25 12:08:00 +0200

    Update create_table script: options for start index and input buffer size; save HDF5 file in chunks and compressed. Update output files.

--- create_table.py
+++ create_table.py
@@ -19,6 +19,7 @@ parser.add_argument('--random_protons', dest = 'random_protons', action = 'store
 parser.add_argument('--resample_factor', dest = 'resample_factor', type = int, required = False, default = -1, help = '' )
 parser.add_argument('-s', '--start', dest = 'start', type = int, required = False, default = -1, help = 'First event to process' )
 parser.add_argument('-n', '--events', dest = 'events', type = int, required = False, default = -1, help = 'Number of events to process' )
+parser.add_argument('--read_size', dest = 'read_size', required = False, default = "150MB" , help = 'Input buffer size.' )
 #parser.add_argument('-v', '--verbose', action = 'store_true', dest = 'verbose', required = False, help = 'Enable verbose' )
 args = parser.parse_args()
 
@@ -42,6 +43,9 @@ print ( "First event to process: {}".format( "All" if firstEvent_ is None else f
 maxEvents_ = None
 if hasattr( args, 'events' ) and args.events > 0: maxEvents_ = args.events
 print ( "Number of events to process: {}".format( "All" if maxEvents_ is None else maxEvents_ ) )
+read_size_ = "150MB"
+if hasattr( args, 'read_size' ): read_size_ = args.read_size
+print ( "Input buffer size: {}".format( read_size_ ) )
 
 resample_ = False
 if resample_factor_ > 1: resample_ = True
@@ -49,6 +53,8 @@ if resample_factor_ > 1: resample_ = True
 entrystop_ = maxEvents_ if firstEvent_ is None else ( firstEvent_ + maxEvents_ )
 
 np.random.seed( 42 )
+
+dset_chunk_size = 50000
 
 columns = ( "Run", "LumiSection", "BX", "EventNum", "CrossingAngle",
             "MultiRP", "Arm", "RPId1", "RPId2", "TrackX1", "TrackY1", "TrackX2", "TrackY2",
@@ -79,108 +85,143 @@ protons_keys[ "Track2ThX_MultiRP" ] = "trackthx_multi2"
 protons_keys[ "Track2ThY_MultiRP" ] = "trackthy_multi2"
 protons_keys[ "ExtraPfCands" ] = "nExtraPfCandPV3"
 
-protons_list = {}
-for col_ in columns:
-    protons_list[ col_ ] = []
-
-selections = None
-counts = None
-
 counts_label_protons_ = "Protons" if not random_protons_ else "ProtonsRnd"
 
-for file_ in fileNames_:
-    print ( file_ ) 
-    root_ = uproot4.open( file_ )
-    tree_ = root_["ggll_miniaod/ntp1"]
-    
-    keys = ["Run", "LumiSection", "BX", "EventNum", "CrossingAngle","nHLT", "HLT_Accept", "HLT_Prescl", "HLT_Name",
-        "nMuonCand", "MuonCand_pt", "MuonCand_eta", "MuonCand_phi", "MuonCand_e", "MuonCand_charge", "MuonCand_vtxz", "MuonCand_istight",
-        "nPrimVertexCand", "PrimVertexCand_z", "PrimVertexCand_chi2", "PrimVertexCand_ndof", "PrimVertexCand_tracks",
-        "Weight", "PUWeightTrue"]
-    keys.append( "nPfCand" )
-    keys.extend( tree_.keys( filter_name="PfCand*" ) ) 
-    keys.append( "nRecoProtCand" )
-    keys.extend( tree_.keys( filter_name="ProtCand*" ) )  
-    print ( keys )
-    
-    for events_ in tree_.iterate( keys , library="ak", how="zip", step_size="150 MB", entry_start=firstEvent_, entry_stop=entrystop_ ):
-        print ( len(events_), events_ )
-        
-        #events_sel_ = select_events( events_, apply_exclusive_ )
-        events_sel_, selections_, counts_ = select_events( events_, apply_exclusive=apply_exclusive_ )
-
-        # Repeat events by resample factor
-        if resample_:
-            counts_ = counts_ * resample_factor_
-
-        if selections is None:
-            selections = selections_
-            counts = counts_
-        else:
-            msk_selections = np.full_like( selections, False, dtype='bool' )
-            for key in selections_:
-                print ( key )
-                print ( selections == key )
-                msk_selections |= ( selections == key )
-            counts[ msk_selections ] += counts_
-
-        # Repeat events by resample factor
-        if resample_:
-            events_sel_ = ak.concatenate( ( [events_sel_] * resample_factor_ ), axis=0 )
-            
-        # Randomize proton arrays
-        if random_protons_:
-            protons_sel_ = events_sel_.ProtCand
-        
-            index_rnd_ = np.random.permutation( len( events_sel_ ) )
-        
-            protons_rnd_ = protons_sel_[ index_rnd_ ]
-        
-            events_sel_[ "ProtCandRnd" ] = protons_rnd_    
-    
-            print ( ak.num( events_sel_.ProtCand ) )
-            print ( ak.num( events_sel_.ProtCandRnd ) )    
-
-        #protons_ = select_protons( events_sel_ )
-        protons_ = None
-        if not random_protons_: protons_ = select_protons( events_sel_, "ProtCand" )
-        else:                   protons_ = select_protons( events_sel_, "ProtCandRnd" )    
-
-        counts_protons_ = len( protons_ )
-        if not counts_label_protons_ in selections:
-            selections = np.concatenate( ( selections, np.array( [ counts_label_protons_ ] ) ) )
-            counts = np.concatenate( ( counts, np.array( [counts_protons_] ) ) )
-        else:    
-            counts[ selections == counts_label_protons_ ] += counts_protons_ 
-
-        print ( selections )
-        print ( counts )
-
-        protons_flatten_ = {}
-        for col_ in columns:
-            protons_list[ col_ ].append( ak.flatten( protons_[ protons_keys[ col_ ] ] ) )
-
-    root_.close()
-
-protons_flatten_all = {}
-for col_ in columns:
-    protons_flatten_all[ col_ ] = np.array( np.concatenate( protons_list[ col_ ] ) )
-    
-print ( "Protons (flattened): {}".format( len( protons_flatten_all["Xi"] ) ) )
-
-data_ = np.stack( list( protons_flatten_all.values() ), axis=1 )
-print ( data_ )
-print ( data_.shape )
-columns_ = np.array( columns, dtype='S' )
-print ( columns_ )
-
-event_counts_ = counts
-print ( event_counts_ )
-selections_ = np.array( selections, dtype='S' )
-print ( selections_ )
-
 with h5py.File( 'output-' + label_ + '.h5', 'w') as f:
-    dset = f.create_dataset( 'protons', data=data_ )
+
+    dset = f.create_dataset( 'protons', ( dset_chunk_size, len( columns ) ), compression="gzip", chunks=True, maxshape=( None , len( columns ) ) )
+    print ( "Initial dataset shape: {}".format( dset.shape ) )
+
+    protons_list = {}
+    for col_ in columns:
+        protons_list[ col_ ] = []
+    
+    selections = None
+    counts = None
+    
+    dset_slice = 0
+    dset_idx = 0
+    dset_entries = 0
+
+    for file_ in fileNames_:
+        print ( file_ ) 
+        root_ = uproot4.open( file_ )
+
+        print ( "Number of events in tree: {}".format( np.array( root_["ggll_miniaod/ntp1/nMuonCand"] ).size ) )
+
+        tree_ = root_["ggll_miniaod/ntp1"]
+ 
+        keys = ["Run", "LumiSection", "BX", "EventNum", "CrossingAngle","nHLT", "HLT_Accept", "HLT_Prescl", "HLT_Name",
+            "nMuonCand", "MuonCand_pt", "MuonCand_eta", "MuonCand_phi", "MuonCand_e", "MuonCand_charge", "MuonCand_vtxz", "MuonCand_istight",
+            "nPrimVertexCand", "PrimVertexCand_z", "PrimVertexCand_chi2", "PrimVertexCand_ndof", "PrimVertexCand_tracks",
+            "Weight", "PUWeightTrue"]
+        keys.append( "nPfCand" )
+        keys.extend( tree_.keys( filter_name="PfCand*" ) ) 
+        keys.append( "nRecoProtCand" )
+        keys.extend( tree_.keys( filter_name="ProtCand*" ) )  
+        print ( keys )
+        
+        for events_ in tree_.iterate( keys , library="ak", how="zip", step_size=read_size_, entry_start=firstEvent_, entry_stop=entrystop_ ):
+            print ( len(events_), events_ )
+            
+            #events_sel_ = select_events( events_, apply_exclusive_ )
+            events_sel_, selections_, counts_ = select_events( events_, apply_exclusive=apply_exclusive_ )
+    
+            # Repeat events by resample factor
+            if resample_:
+                counts_ = counts_ * resample_factor_
+    
+            if selections is None:
+                selections = selections_
+                counts = counts_
+            else:
+                msk_selections = np.full_like( selections, False, dtype='bool' )
+                for key in selections_:
+                    msk_selections |= ( selections == key )
+                counts[ msk_selections ] += counts_
+    
+            # Repeat events by resample factor
+            if resample_:
+                events_sel_ = ak.concatenate( ( [events_sel_] * resample_factor_ ), axis=0 )
+                
+            # Randomize proton arrays
+            if random_protons_:
+                protons_sel_ = events_sel_.ProtCand
+            
+                index_rnd_ = np.random.permutation( len( events_sel_ ) )
+            
+                protons_rnd_ = protons_sel_[ index_rnd_ ]
+            
+                events_sel_[ "ProtCandRnd" ] = protons_rnd_    
+        
+                print ( ak.num( events_sel_.ProtCand ) )
+                print ( ak.num( events_sel_.ProtCandRnd ) )    
+    
+            #protons_ = select_protons( events_sel_ )
+            protons_ = None
+            if not random_protons_: protons_ = select_protons( events_sel_, "ProtCand" )
+            else:                   protons_ = select_protons( events_sel_, "ProtCandRnd" )    
+    
+            counts_protons_ = len( protons_ )
+            if not counts_label_protons_ in selections:
+                selections = np.concatenate( ( selections, np.array( [ counts_label_protons_ ] ) ) )
+                counts = np.concatenate( ( counts, np.array( [counts_protons_] ) ) )
+            else:    
+                counts[ selections == counts_label_protons_ ] += counts_protons_ 
+    
+            print ( selections )
+            print ( counts )
+    
+            for col_ in columns:
+                protons_list[ col_ ] = np.array( ak.flatten( protons_[ protons_keys[ col_ ] ] ) )
+
+            arr_size_ = len( protons_list[ "Xi" ] )
+            print ( "Flattened array size: {}".format( arr_size_ ) )
+
+            dset_entries += arr_size_
+
+            if dset_entries > dset_chunk_size:
+                resize_factor_ = ( dset_entries // dset_chunk_size )
+                chunk_resize_  = resize_factor_ * dset_chunk_size
+
+                print ( "Resizing output dataset by {} entries.".format( chunk_resize_ ) )
+                dset.resize( ( dset.shape[0] + chunk_resize_ ), axis=0 )
+                print ( "Dataset shape: {}".format( dset.shape ) )
+                        
+                dset_slice += resize_factor_
+                # Count the rest to the chunk size 
+                dset_entries = ( dset_entries % dset_chunk_size )
+
+            print ( "Stacking data." )
+            data_ = np.stack( list( protons_list.values() ), axis=1 )
+            print ( data_.shape )
+            print ( data_ )
+
+            dset_idx_next_ = dset_idx + arr_size_
+            print ( "Slice: {}".format( dset_slice ) )
+            print ( "Writing in positions ({},{})".format( dset_idx, dset_idx_next_ ) )
+            dset[ dset_idx : dset_idx_next_ ] = data_
+            dset_idx = dset_idx_next_ 
+
+        # Iteration on input files
+        root_.close()
+    
+    # Reduce dataset to its final size 
+    print ( "Reduce dataset." )
+    dset.resize( ( dset_idx ), axis=0 ) 
+    print ( "Dataset shape: {}".format( dset.shape ) )
+
+    print ( "Writing column names and event counts.")
+
+    columns_ = np.array( columns, dtype='S' )
+    print ( columns_ )
+    
+    event_counts_ = counts
+    print ( event_counts_ )
+
+    selections_ = np.array( selections, dtype='S' )
+    print ( selections_ )
+
     dset_columns = f.create_dataset( 'columns', data=columns_ )
     dset_counts = f.create_dataset( 'event_counts', data=event_counts_ )
     dset_selections = f.create_dataset( 'selections', data=selections_ )
@@ -193,3 +234,4 @@ with h5py.File( 'output-' + label_ + '.h5', 'w') as f:
     print ( list( dset_counts ) )
     print ( dset_selections )
     print ( list( dset_selections ) )
+
--- sub_condor.csh
+++ sub_condor.csh
@@ -4,10 +4,12 @@ source /afs/cern.ch/work/a/antoniov/env/uproot4/bin/activate.csh
 
 set file=$1
 set label=$2
-set option1=$3
-set option2=$4
-set option3=$5
-set option4=$6
+set option1="$3"
+set option2="$4"
+set option3="$5"
+set option4="$6"
+set option5="$7"
+set option6="$8"
 set EXEC=/eos/home-a/antoniov/SWAN_projects/pps-dilepton-analysis
 set OUTPUT=/eos/home-a/antoniov/SWAN_projects/pps-dilepton-analysis/output
 echo "file: "$file
@@ -16,6 +18,8 @@ echo "option: "$option1
 echo "option: "$option2
 echo "option: "$option3
 echo "option: "$option4
+echo "option: "$option5
+echo "option: "$option6
 echo $EXEC
 echo $OUTPUT
 
@@ -30,6 +34,6 @@ env
 echo $PWD
 
 echo 'Running...'
-echo python $EXEC/create_table.py --files=$file --label=$label $option1 $option2 $option3 $option4
-python $EXEC/create_table.py --files=$file --label=$label $option1 $option2 $option3 $option4
+echo python $EXEC/create_table.py --files=$file --label=$label $option1 $option2 $option3 $option4 $option5 $option6
+python $EXEC/create_table.py --files=$file --label=$label $option1 $option2 $option3 $option4 $option5 $option6
 cp *.h5 $OUTPUT
